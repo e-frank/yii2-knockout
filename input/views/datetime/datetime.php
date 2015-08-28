4afc4f7083206086a0ef1cad5a5302a2155c9542
Elmar Frank <elmar.frank@efrank.at>
2015-08-28 19:42:41 +0200

    updated datetime view

--- input/views/datetime/datetime.php
+++ input/views/datetime/datetime.php
@@ -4,14 +4,16 @@
 			<input class="form-control" maxlength="<?= $maxlength ?>" size="<?= $size ?>" data-bind="'value': <?= $attribute ?>.display" />
 		</div>
 
-<? if ($time == true) { ?>
-		<select class="form-control" data-bind="options: <?= $attribute ?>.times, optionsCaption: '-', value: <?= $attribute ?>.time"></select>
-<? } else { ?>
+<? if ($time !== true) { ?>
 		<select class="form-control" data-bind="options: <?= $attribute ?>.hours, optionsValue: 'key', optionsText: 'value', optionsCaption: '-', value: <?= $attribute ?>.hour"></select>
-		<select class="form-control" data-bind="options: <?= $attribute ?>.minutes, optionsValue: 'key', optionsText: 'value', optionsCaption: '-', value: <?= $attribute ?>.minute"></select>
 <? } ?>
 
 		<div class="input-group">
+<? if ($time == true) { ?>
+		<select class="form-control" data-bind="options: <?= $attribute ?>.times, optionsCaption: '-', value: <?= $attribute ?>.time"></select>
+<? } else { ?>
+		<select class="form-control" data-bind="options: <?= $attribute ?>.minutes, optionsValue: 'key', optionsText: 'value', optionsCaption: '-', value: <?= $attribute ?>.minute"></select>
+<? } ?>
 			<span class="input-group-addon" data-bind="click: <?= $attribute ?>.clear">
 				<a href="javascript: false;" class="text-danger" ><i class="fa fa-remove"></i></a>
 			</span>
@@ -19,9 +21,5 @@
 				<a href="javascript: false;" class="text-primary" ><i class="fa fa-clock-o"></i></a>
 			</span>
 		</div>
-</div>
 
-<hr>
-<ul data-bind="foreach: <?= $attribute ?>.times">
-	<li data-bind="text: $data"></li>
-</ul>
+</div>
