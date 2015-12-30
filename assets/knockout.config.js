45b7d9fa6720e1c991a489080e89c61446d40c2a
Elmar Frank <elmar.frank@efrank.at>
2015-12-30 16:11:57 +0100

    knockout observable withPausing

--- assets/knockout.config.js
+++ assets/knockout.config.js
@@ -12,3 +12,19 @@ ko.observable.fn.toString = function() {
 ko.computed.fn.toString = function() {
 	return "KO.COMPUTED: " + ko.toJSON(this(), null, 2);
 };
+
+ko.observable.fn.withPausing = function() {
+    this.notifySubscribers = function() {
+       if (!this.pauseNotifications) {
+          ko.subscribable.fn.notifySubscribers.apply(this, arguments);
+       }
+    };
+
+    this.sneakyUpdate = function(newValue) {
+        this.pauseNotifications = true;
+        this(newValue);
+        this.pauseNotifications = false;
+    };
+
+    return this;
+};
